3c6d38ad2b2f2137e806819eae076539c18e9695
Ember <giden74916@gmail.com>
2017-05-12 21:44:10 +0800

    Finish “Orange and Green blocks in landscape and portrait”

---
+++ OrangeAndGreenBlocks/OrangeAndGreenBlocks/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OrangeAndGreenBlocks
+//
+//  Created by Ember on 2017/5/12.
+//  Copyright © 2017年 Ember. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+   
+}
+
