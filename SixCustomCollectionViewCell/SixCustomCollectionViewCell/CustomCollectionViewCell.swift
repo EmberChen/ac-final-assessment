6b949c35140c276adb107719b5d3262730145598
Ember <giden74916@gmail.com>
2017-05-14 13:26:19 +0800

    1.Finish “6 custom cells in landscape and protrait mode” 2.Finish “Color switch”

---
+++ SixCustomCollectionViewCell/SixCustomCollectionViewCell/CustomCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCollectionViewCell.swift
+//  SixCustomCollectionViewCell
+//
+//  Created by Ember on 2017/5/14.
+//  Copyright © 2017年 Ember. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
--- SixCustomCollectionViewCell/SixCustomCollectionViewCell/ViewController.swift
+++ SixCustomCollectionViewCell/SixCustomCollectionViewCell/ViewController.swift
@@ -8,18 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
 
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var label: UILabel!
+    
+    var isBlue = true
+    var contentText = ["Alert view","Color switch","Core motion","App's setting page","Navigator","Mail"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return contentText.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCollectionViewCell
+        cell.label.text = contentText[indexPath.row]
+     
+        return cell
+    }
+    
+    //to decide the cell size
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if traitCollection.horizontalSizeClass == .compact && traitCollection.verticalSizeClass == .regular{
+        
+            return CGSize(width: (view.frame.width/2)-1, height: (view.frame.height/3)-1)
+        }else{
+        
+            return CGSize(width: (view.frame.width/3)-1, height: (view.frame.height/2)-1)
+        }
+    }
+    
+    //reload cell when rotate screen
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        collectionView.reloadData()
     }
 
-
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
+        
+        if indexPath.row == 1{
+            print(indexPath)
+            
+            let cell = collectionView.cellForItem(at: indexPath) as! CustomCollectionViewCell
+            if isBlue == true{
+                cell.backgroundColor = UIColor.red
+                cell.label.textColor = UIColor.white
+                isBlue = false
+            }else{
+                cell.backgroundColor = UIColor.blue
+                cell.label.textColor = UIColor.white
+                isBlue = true
+            }
+        }
+    }
+    
+    
+    func colorSwitch(color:UIColor) -> UIColor{
+        if color == UIColor.blue{
+            return UIColor.red
+        }else{
+            return UIColor.blue
+        }
+    }
+    
 }
 
